d5706ce77268c18671355ca069906f11546a79d1
Jared Jaimes <jj.jaredjaimes@gmail.com>
2024-12-05 17:43:38 -0700

    debug run

--- service/index.js
+++ service/index.js
@@ -10,7 +10,7 @@ let scores = [];
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
-app.use(express.json());
+
 
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
